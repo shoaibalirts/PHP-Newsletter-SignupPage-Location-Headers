3234e7205e3026912b128749faab997bc61101eb
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-03 21:41:45 +0100

    checkbox validated then redirect page to thankyou.php. preserved the entered name and email too

---
+++ thankyou.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="simple.css">
+    <title>Thank you</title>
+</head>
+<body>
+    <header><h1>Sign up for the newsletter</h1></header>
+    <main>
+        <h1>Thank you for signing up!</h1>
+    </main>
+</body>
+</html>
