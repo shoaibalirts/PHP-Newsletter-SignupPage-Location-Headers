60783fa1b1657e07da5e6b2fe84c30eacfa3b244
shoaibalirts <shoaibali.rts@gmail.com>
2025-01-03 22:13:09 +0100

    added not found page

---
+++ notfound.php
@@ -0,0 +1,23 @@
+<?php 
+    http_response_code(404);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="simple.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>Signup newsletter website</h1> 
+    </header>
+    <main>
+        <h1>Page not found!</h1>
+    </main>
+</body>
+
+</html>
